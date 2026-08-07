4d284f97bde5fa6ee8706a2054aa706412677979
KarnNA10 <jackyel9911@gmail.com>
2026-08-07 12:14:18 -0600

    Update tableGen.py

--- Other/tableGen.py
+++ Other/tableGen.py
@@ -1,9 +1,9 @@
-a = 33
-link = "https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/"
+a = 44 #Ultimo plano
+link = "https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/"
 b = []
 
 
-for i in range(1,a,3):
+for i in range(35,a+1,3):
     if i<10:
         c="00"
     else: 
@@ -33,6 +33,10 @@ debe quedar algo así:
 |028![028](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/028.png)|029![029](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/029.png)|030![030](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/030.png)|
 |031![031](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/031.png)|032![032](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/032.png)|033![033](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Preview/033.png)|
 
+|035![035](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/035.png)|036![036](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/036.png)|037![037](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/037.png)|
+|038![038](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/038.png)|039![039](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/039.png)|040![040](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/040.png)|
+|041![041](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/041.png)|042![042](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/042.png)|043![043](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/043.png)|
+|044![044](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/044.png)|045![045](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/045.png)|046![046](https://github.com/KarnNA10/Animatic-TheMonk/blob/main/Planos/Storyboard/046.png)|
 
 Soy el capitán Clark y estos son mis backrooms
 """
